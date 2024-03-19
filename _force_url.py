f3ca1c494f7c887d3d7d4a849138b1974581292c
Ylan <hei.nalisoa@gmail.com>
2024-03-19 20:48:59 +0300

    feat: get success response from success url

--- _force_url.py
+++ _force_url.py
@@ -7,65 +7,72 @@ import random
 import os
 import aiofiles
 
-
 TARGET_URL = "http://127.0.0.1:5000"
 
 
 async def write_to_file(directory, filename, content):
-    async with aiofiles.open(os.path.join(directory, filename), "w") as file:  # Ouvrir le fichier de manière asynchrone
-        await file.write(content)  # Écrire le contenu dans le fichier
+    async with aiofiles.open(os.path.join(directory, filename), "w") as file:
+        await file.write(content)
 
 
-# Fonction pour récupérer le contenu d'un fichier
+async def write_successful_response(directory, filename, response_content):
+    async with aiofiles.open(os.path.join(directory, filename), "w") as file:
+        await file.write(response_content)
+
+
 def retrieve_file_content(file_path):
     data = None
-    with open(file_path, 'r') as file:  # Ouvrir le fichier en mode lecture
-        data = file.read()  # Lire le contenu du fichier
+    with open(file_path, 'r') as file:
+        data = file.read()
     return data
 
 
-# Fonction asynchrone pour trouver les routes existantes
 async def find_existing_routes(routes, workers_count):
     available_routes = ""
     async with aiohttp.ClientSession() as session:
         for route in routes:
-            print("processing... %s/%s" % (TARGET_URL, route))  # Afficher un message de traitement en cours avec les valeurs de route et TARGET_URL
-            async with session.get(f"{TARGET_URL}/{route}") as res:  # Effectuer une requête HTTP GET
-                if res.status != 404:  # Vérifier si la réponse est différente de 404 (non trouvé)
-                    random_value = random.randint(0, 1000)  # Générer une valeur aléatoire
-                    available_routes += route + "\n"  # Ajouter la route disponible à la chaîne
-                    async with session.get(f"{TARGET_URL}/{route}/{random_value}") as path:  # Requête pour un chemin spécifique
-                        if path.status != 404:  # Vérifier si le chemin existe
-                            available_routes += f"{route}/:id \n"  # Ajouter le chemin à la chaîne
-                    for r in routes:  # Parcourir les routes
-                        async with session.get(f"{TARGET_URL}/{route}?{r}={random_value}") as query:  # Requête pour une requête spécifique
-                            if query.status != 404:  # Vérifier si la requête est valide
-                                available_routes += f"{route}?{r}=? \n"  # Ajouter la requête à la chaîne
+            print("processing... %s/%s" % (TARGET_URL, route))
+            async with session.get(f"{TARGET_URL}/{route}") as res:
+                if res.status != 404:
+                    random_value = random.randint(0, 1000)
+                    available_routes += route + "\n"
+                    async with session.get(f"{TARGET_URL}/{route}/{random_value}") as path:
+                        if path.status != 404:
+                            available_routes += f"{route}/:id \n"
+                            response_content = await path.text()
+                            await write_successful_response("routes_availables", f"{route}_id_response.txt", response_content)
+                    for r in routes:
+                        async with session.get(f"{TARGET_URL}/{route}?{r}={random_value}") as query:
+                            if query.status != 404:
+                                available_routes += f"{route}?{r}=? \n"
+                                response_content = await query.text()
+                                filename = str(uuid.uuid4()) + "_success_response.txt"
+                                await write_successful_response("routes_availables", filename, response_content)
 
     print("proc number: %s" % workers_count)
-    filename = str(uuid.uuid4()) + ".txt"  # Générer un nom de fichier unique
-    directory = "routes_availables"  # Chemin du répertoire
-    if not os.path.exists(directory):  # Vérifier si le répertoire n'existe pas
-        os.makedirs(directory)  # Créer le répertoire s'il n'existe pas
+    filename = str(uuid.uuid4()) + "_success_url.txt"
+    directory = "routes_availables"
+    if not os.path.exists(directory):
+        os.makedirs(directory)
     await write_to_file(directory, filename, available_routes)
 
 
 async def main():
-    random_routes = retrieve_file_content("dir_list.txt").split("\n")  # Lire les routes à partir d'un fichier
-    workers_count = multiprocessing.cpu_count()  # Obtenir le nombre de processeurs
+    random_routes = retrieve_file_content("dir_list.txt").split("\n")
+    workers_count = multiprocessing.cpu_count()
 
-    length = len(random_routes)  # Obtenir la longueur des routes
-    quota = math.floor(length / workers_count)  # Calculer le quota de routes par processus
-    index_from = 0  # Index de départ
+    length = len(random_routes)
+    quota = math.floor(length / workers_count)
+    index_from = 0
 
-    tasks = []  # Liste pour stocker les tâches
-    for i in range(0, workers_count):  # Boucler sur le nombre de processus
-        batch = random_routes[index_from:index_from+quota]  # Diviser les routes en lots pour chaque processus
-        tasks.append(find_existing_routes(batch, workers_count))  # Ajouter la tâche à la liste
-        index_from = index_from + quota  # Mettre à jour l'index de départ
+    tasks = []
+    for i in range(0, workers_count):
+        batch = random_routes[index_from:index_from+quota]
+        tasks.append(find_existing_routes(batch, workers_count))
+        index_from = index_from + quota
 
-    await asyncio.gather(*tasks)  # Exécuter toutes les tâches en parallèle
+    await asyncio.gather(*tasks)
 
 
 if __name__ == "__main__":
-    asyncio.run(main())  # Exécuter la fonction principale de manière asynchrone
+    asyncio.run(main())
